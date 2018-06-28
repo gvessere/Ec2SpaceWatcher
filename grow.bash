6db5fb50c35243286d1c060d44615347f139c38f
Gery Vessere <gvessere@illumina.com>
2018-06-28 09:50:33 -0400

    add more drives if there is only one disks in the instance, adjust total size so we make the most out of the local drive

--- grow.bash
+++ grow.bash
@@ -14,8 +14,23 @@ let COL=1+$MDDRIVES
 DISKSIZE=`echo $DISKSIZES | cut -d" " -f$COL`
 
 # if no md drive and at least 2 local drives, then raid those, else add more disks
-if [[ (($LVDISPLAY -eq 0) && ((-e /dev/xvdb) || (-e /dev/nvme1n1) || (-e /dev/sdb))) ]]; then
+if [[ (($LVDISPLAY -eq 0) && ((-e /dev/xvdb && -e /dev/xvdc) || (-e /dev/nvme1n1 && -e /dev/nvme2n1) || (-e /dev/sdb && -e /dev/sdc))) ]]; then
     echo "skip adding EBS drives this first time around, gonna use instance store"
+elif [[ (($LVDISPLAY -eq 0) && ((-e /dev/xvdb) || (-e /dev/nvme1n1) || (-e /dev/sdb))) ]]; then
+
+    if [[ -e /dev/xvdb ]]; then
+        DEVICE=/dev/xvdb
+    elif [[ -e /dev/nvme1n1 ]];
+        DEVICE=/dev/nvme1n1
+    else
+        DEVICE=/dev/sdb
+    fi
+
+    # decrement total disks and recompute total size
+    # to make the most out of the local drive
+    let DISKCOUNT=$DISKCOUNT-1
+    let DISKSIZE=(`blockdev --getsize64 $DEVICE`/1024/1024-2)*$DISKCOUNT
+    ./attachdrives.bash $DISKSIZE $DISKCOUNT
 else
     ./attachdrives.bash $DISKSIZE $DISKCOUNT
 fi
